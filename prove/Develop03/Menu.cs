d9168bd52e48fba1cec664535d5508745befbe89
berrypatchwhims <berrypatchgirl@myyahoo.com>
2024-10-03 09:19:14 -0700

    W03 Scripture mem

---
+++ prove/Develop03/Menu.cs
@@ -0,0 +1,18 @@
+using System;
+public class Menu
+{
+
+    private string _welcomeMessage;
+    private string _beginMemorizing;
+    private string _quit;
+
+    public string DisplayWelcomeMessage()
+    {
+        //needs to contain directions about "enter" and quit
+        _welcomeMessage = "";
+        
+        return _welcomeMessage;
+    }
+
+
+}
